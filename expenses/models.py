6ed503a080e0a21a373917b81c668fb9f9606712
JumaKahiga <kabirumwangi@gmail.com>
2019-12-13 20:31:36 +0300

    Saving WIP

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    uuid = models.CharField(max_length=250, primary_key=True)
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField()
+    amount = models.PositiveIntegerField()
+    currency = models.CharField(max_length=10)
+    approved = models.BooleanField(default=False)
+    employee = models.ForeignKey(
+        'Employee', related_name='employee_expenses',
+        on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    uuid = models.CharField(max_length=250, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
